512f3df315936e34105094595b7f9de59c54597a
Rishabh Jain <rishabh@jnexsoft.com>
2019-07-15 18:54:11 +0530

    added mongoose-timestamp plugin

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
+import mongooseTimestamp from 'mongoose-timestamp';
 
 mongoose.connect('mongodb://user:password123@ds347467.mlab.com:47467/chat', {useNewUrlParser: true});
 
+mongoose.plugin(mongooseTimestamp, {
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
+});
 
 export default mongoose;
