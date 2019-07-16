4190fa19c926ebfe2b4eb985c36bc5c8e5e6a701
Rishabh Jain <rishabh@jnexsoft.com>
2019-07-16 17:32:56 +0530

    findoneorcreate

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import mongooseTimestamp from 'mongoose-timestamp';
+import mongooseFindoneorcreate from 'mongoose-findoneorcreate';
 
 mongoose.connect('mongodb://user:password123@ds347467.mlab.com:47467/chat', {useNewUrlParser: true});
 
@@ -7,5 +8,7 @@ mongoose.plugin(mongooseTimestamp, {
   createdAt: 'created_at',
   updatedAt: 'updated_at'
 });
+
+mongoose.plugin(mongooseFindoneorcreate);
 
 export default mongoose;
