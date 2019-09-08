017e70445bd888ec39e52ec2e47214839e746976
Rishabh Jain <rishabh@jnexsoft.com>
2019-09-08 16:47:55 +0530

    feat(app): enhanced app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
+import { Socket } from "socket.io";
+import ChatController from "controllers/ChatController";
+import UserController from "controllers/UserController";
+
 export const events = {
   createUser: 'create-user',
   createChat: 'create-chat',
@@ -5,3 +9,19 @@ export const events = {
   listMessages: 'list-messages',
   newMessage: 'new-message',
 };
+
+let socket: Socket;
+
+export const app = {
+  events,
+  socket: null as any as Socket,
+  setSockets($socket: Socket) {
+    socket = $socket;
+    this.socket = socket;
+    ChatController.setSockets(this.socket);
+    UserController.setSockets(this.socket);
+  }
+};
+
+
+export default app;
