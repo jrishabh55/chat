202cea353babe6bbc7dbef8bff529c17bc7a0a65
Rishabh Jain <rishabh@jnexsoft.com>
2019-09-09 01:31:21 +0530

    chat controller new format

--- src/controllers/ChatController.ts
+++ src/controllers/ChatController.ts
@@ -3,14 +3,15 @@ import { autobind } from 'core-decorators';
 import { events } from 'app';
 import { Types } from 'db';
 import Chat, { IChat } from 'models/Chat';
-import { ICreateChatData } from 'interfaces/Chat';
+import { ICreateChatData, INewMessage } from 'interfaces/Chat';
 import User from 'models/User';
+import Message from 'models/Message';
 import { Controller } from 'controllers/Controller';
 
 @autobind
 export class ChatController extends Controller {
 
-  public socket: Socket;
+  protected chatRooms: { [key: string]: Socket } = {};
 
   public async create(data: ICreateChatData) {
     const { name, users } = data.chat;
@@ -27,10 +28,15 @@ export class ChatController extends Controller {
         users: $users.map(u => u.id)
       });
 
+      const chatRoom = `chat-${$chat.id}`;
+
       this.socket.emit(events.createChat, {
         status: 'ok',
         chat: $chat.toJSON()
       });
+
+      this.setupChatRoom(chatRoom);
+      this.setupChatRoomMessage(chatRoom);
 
     } else {
       this.socket.emit(events.createChat, {
@@ -42,6 +48,27 @@ export class ChatController extends Controller {
         ]
       });
     }
+  }
+
+  public async setupChatRoomMessage(chatRoom: string) {
+    this.chatRooms[chatRoom].on(events.newMessage, this.newChatMessage);
+  }
+
+  public async newChatMessage(data: INewMessage) {
+    const { chatRoom, message, user } = data;
+    const $chat = await Message.create({
+      user_id: user.id,
+      message: message.value,
+      type: message.type,
+      chat_id: chatRoom.split('_')[1],
+    });
+
+    this.chatRooms[chatRoom].emit($chat.toJSON());
+  }
+
+  public async setupChatRoom(chatRoom: string) {
+    this.socket.join(chatRoom);
+    this.chatRooms[chatRoom] = this.socket.in(chatRoom);
   }
 }
 
