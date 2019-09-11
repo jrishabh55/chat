74240deebc482675ff6a805b55458e835679c81e
Rishabh Jain <rishabh@jnexsoft.com>
2019-09-12 01:15:03 +0530

    fix(user): Fixed create-user event name

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Controller } from 'controllers/Controller';
 import { autobind } from 'core-decorators';
 import User, { IUser } from 'models/User';
 import { IUserData } from 'interfaces/Chat';
+import { events } from 'app';
 
 @autobind
 export class UserController extends Controller {
@@ -9,7 +10,7 @@ export class UserController extends Controller {
   public async createOrFindUser(chatData: IUserData) {
     const { user } = chatData;
     const $user = await this.create({ id: user.externalUserId });
-    this.emit('new-user', { user: $user.toJSON() });
+    this.emit(events.createChat, { user: $user.toJSON() });
   }
 
   private async create(user: IUser | any) {
